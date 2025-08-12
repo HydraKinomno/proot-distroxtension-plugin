9a20611557d79ed6f0182bdb05d88b2d71ca7214
beef flavoured chicken <gameejay2662@outlook.com>
2025-08-13 00:13:02 +0600

    Create raspberryos.sh

---
+++ scripts/raspberryos.sh
@@ -0,0 +1,4 @@
+DISTRO_NAME="Raspbian"
+DISTRO_COMMENT="Raspberry Pi OS Plugin for proot-distro'
+TARBALL_URL['aarch64']="https://downloads.raspberrypi.org/raspios_lite_arm64/root.tar.xz"
+TARBALL_SHA256['aarch64']="f34802fbb300b4d088a638c638683fd2bfc1c03f4b40fa4cb7d2113231401a21"
