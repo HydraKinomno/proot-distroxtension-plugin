ab1969923dfc7d18c3c33aa36bec399aec866e14
beef flavoured chicken <gameejay2662@outlook.com>
2025-08-13 00:18:44 +0600

    Update raspberryos.sh

--- scripts/raspberryos.sh
+++ scripts/raspberryos.sh
@@ -1,4 +1,4 @@
 DISTRO_NAME="Raspbian"
 DISTRO_COMMENT="Raspberry Pi OS Plugin for proot-distro'
 TARBALL_URL['aarch64']="https://downloads.raspberrypi.org/raspios_lite_arm64/root.tar.xz"
-TARBALL_SHA256['aarch64']="f34802fbb300b4d088a638c638683fd2bfc1c03f4b40fa4cb7d2113231401a21"
+TARBALL_SHA256['aarch64']="62d025b9bc7ca0e1facfec74ae56ac13978b6745c58177f081d39fbb8041ed45"
