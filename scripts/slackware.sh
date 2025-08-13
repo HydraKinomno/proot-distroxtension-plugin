527845bcb8676f2db3009db39f417a4970e2b16e
beef flavoured chicken <gameejay2662@outlook.com>
2025-08-14 01:03:17 +0600

    Add files via upload

---
+++ scripts/slackware.sh
@@ -0,0 +1,3 @@
+DISTRO_NAME="Slackware ARM"
+TARBALL_URL['aarch64']="https://slackware.uk/slackwarearm/slackwarearm-devtools/minirootfs/roots/slackarm-15.0-miniroot_15Feb22.tar.xz"
+TARBALL_SHA256['aarch64']="80c2095392444ffbfe5168fa9a96119a6dab2cea35d24a0a727b7eead2af0e54"
