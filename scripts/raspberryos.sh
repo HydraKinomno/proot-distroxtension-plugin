ee1a2a98792eaa1aea41813e92a23823fe65fed1
beef flavoured chicken <gameejay2662@outlook.com>
2025-08-15 14:03:42 +0600

    Delete scripts/raspberryos.sh

--- scripts/raspberryos.sh
+++
@@ -1,4 +0,0 @@
-DISTRO_NAME="Raspbian"
-DISTRO_COMMENT="Raspberry Pi OS Plugin for proot-distro"
-TARBALL_URL['aarch64']="https://downloads.raspberrypi.org/raspios_lite_arm64/root.tar.xz"
-TARBALL_SHA256['aarch64']="db1b538171f40bc5f8980e3ce8153cf840627351e9c5dc2e5862f1284bc36c4b"
